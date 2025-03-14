55e6d05808fb18ec13ac940f5ced15c6d9887faf
wenay12 <wenayr@gmail.com>
2025-03-15 01:39:08 +0300

    1212

--- src/common/src/applyTransactionAsyncUpdate.tsx
+++ src/common/src/applyTransactionAsyncUpdate.tsx
@@ -65,63 +65,6 @@ export function getUpdateTable<T>(
 
     }
 }
-export function applyTransactionAsyncUpdate2<T>(
-    grid: GridReadyEvent<T, any> | null | undefined,
-    newData: (Partial<T>)[],
-    getId: (...a: any[]) => string,
-    bufTable: { [id: string]: Partial<T> },
-    option?: options
-) {
-    // Установка параметров (объединение переданных опций с настройками по умолчанию)
-    const op = {...optionsDef, ...(option ?? {})};
-
-    // Проверка наличия сетки и доступа к данным через API
-    if (grid?.api.getRowNode) {
-        const arrNew: T[] = []; // Массив для новых данных (добавляемые строки)
-
-        // Основная обработка данных (map используется для обновления или создания записей)
-        const arr = newData.map(e => {
-            // Получение ID для текущей строки
-            const id = getId(e);
-
-            const newData= { ...(bufTable[id] ?? {}), ...e } as T
-            if (op.updateBuffer) bufTable[id] = newData
-            // Попытка найти существующую строку по ID
-            const a = grid.api.getRowNode(id)?.data;
-
-            if (!a) {
-                // Если строка не найдена - добавить в массив для добавления
-                arrNew.push(newData);
-                return null; // Новая строка обрабатывается отдельно
-            }
-
-            // Если строка найдена - обновить данные в буфере
-            return newData;
-        }) // Убираем `null` и оставляем только существующие строки
-            .filter(e => e) as T[];
-
-        // Добавление новых строк (если есть элементы)
-        if (arrNew.length && op.update) {
-            // добавление элементов должно быть синхронно, т.к. может прейти его update до исполнения
-            grid.api.applyTransaction({ add: arrNew });
-        }
-
-        // Асинхронное обновление существующих строк
-        if (arr.length && op.add) {
-            grid.api.applyTransactionAsync({ update: arr });
-        }
-    } else {
-        if (map.has(bufTable)) map.set(bufTable, new Set())
-        const m = map.get(bufTable)
-
-        newData.forEach(e => {
-            // Получение ID для текущей строки
-            const id = getId(e);
-            m.add(id)
-            if (op.updateBuffer) bufTable[id] = { ...(bufTable[id] ?? {}), ...e } as T
-        }) // Убираем `null` и оставляем только существующие строки
-    }
-}
 
 type UnUndefined<T extends (any | undefined)> = T extends undefined ? never : T
 type t1<T = any> = ColDef<T>["comparator"]
